6dc2e578455dd863fb008a8b0d11224db1732122
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-14 22:39:34 +0100

    add basic button to story

--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import Button from './button'
 
-export default {
-  title: 'Button',
-  component: Button,
-};
+
 
 export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
 
@@ -16,3 +13,8 @@ export const Emoji = () => (
     </span>
   </Button>
 );
+
+export default {
+  component: Button,
+  title: 'Button',
+};
