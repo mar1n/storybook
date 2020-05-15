e7b94cbc10d134f5d814781c291539f11c641caf
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-15 22:46:34 +0100

    svg support

--- components/atoms/button/button.jsx
+++ components/atoms/button/button.jsx
@@ -3,20 +3,62 @@ import { node, string, func } from "prop-types"
 
 import StyledButton, { StyledLinkButton } from "./button.styles"
 
-const Button = ({ children, href, onClick, variant }) => {
-  if (!href) return <StyledButton onClick={onClick} variant={variant} >{children}</StyledButton>;
-  return <StyledLinkButton href={href} variant={variant}>{children}</StyledLinkButton>;
+import IconBag from "../../../assets/images/icons/shopping-bag.svg"
+import IconCart from "../../../assets/images/icons/shopping-cart.svg"
+import IconPlus from "../../../assets/images/icons/plus.svg"
+import IconUser from "../../../assets/images/icons/user.svg"
+import IconX from "../../../assets/images/icons/x.svg"
+
+const Icons = {
+  bag: IconBag,
+  cart: IconCart,
+  plus: IconPlus,
+  user: IconUser,
+  x: IconX
+}
+
+const Button = ({ children, href, icon, onClick, variant }) => {
+  if (!href) return (
+  <StyledButton onClick={onClick} variant={variant} >
+    {icon && <ButtonIcon name={icon} />}
+    {children}
+    </StyledButton>
+  )
+  return (
+  <StyledLinkButton href={href} variant={variant}>
+    {icon && <ButtonIcon name={icon} />}
+    {children}
+    </StyledLinkButton>
+  )
 };
+
+
 
 
 Button.propTypes = {
   children: node.isRequired,
   href: string,
+  icon: string,
   onClick: func,
 }
 
 Button.defaultProps = {
+  children: "Button text",
   variant: "primary"
+}
+
+const ButtonIcon = ({ name }) => {
+  if(Icons[name] === undefined) return null
+  const Icon = Icons[name]
+  return (
+    <span className="button__icon">
+      <Icon />
+    </span>
+  )
+}
+
+Button.propTypes = {
+  name: string.isRequired
 }
 
 export default Button;
--- components/atoms/button/button.stories.js
+++ components/atoms/button/button.stories.js
@@ -8,7 +8,15 @@ const buttonClicked = e => {
   alert("Hello")
 }
 
-export const basicButton = () => <Button variant="primary">Basic Button</Button>
+export const basicButton = () => <Button>Basic button</Button>;
+
+export const secondaryButton = () => (
+	<Button variant="secondary">Secondary button</Button>
+)
+export const tertiaryButton = () => (
+	<Button variant="tertiary">Tertiary button</Button>
+)
+export const iconButton = () => <Button icon="plus">Icon button</Button>
 export const functionButtpn = () => (<Button onClick={buttonClicked}>Function Button</Button>)
 export const linkButton = () => <Button href="/route">Linked Button</Button>
 
