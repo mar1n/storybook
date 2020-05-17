a7a2a3eb10349cfb6b63ff3fb44676fbdcd94af7
mar1n <30738216+mar1n@users.noreply.github.com>
2020-05-17 23:02:20 +0100

    apollo provider graphql

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,15 +2,20 @@ import React from "react"
 import { addDecorator, addParameters } from "@storybook/react"
 import { ThemeProvider } from "styled-components"
 import themeDefault from "../components/particles/themeDefault"
+
+
+import ApolloWrapper from "../components/particles/apollo/provider"
 import GlobalStyles from "../components/particles/globalStyles"
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks'
 
 const GlobalWrapper = (storyFn) => (
   <div>
+    <ApolloWrapper>
     <ThemeProvider theme={themeDefault}>
       <GlobalStyles />
       {storyFn()}
     </ThemeProvider>
+    </ApolloWrapper>
   </div>
 );
 
--- components/pages/homepage/homepage.jsx
+++ components/pages/homepage/homepage.jsx
@@ -1,16 +1,38 @@
-import React from "react"
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import Banner from "../../organisms/banner/banner";
+import Carousel from "../../organisms/carousel/carousel";
+import Footer from "../../organisms/footer/footer";
+import Header from "../../organisms/header/header";
+import { gql } from 'apollo-boost';
 
-import Banner from "../../organisms/banner/banner"
-import Carousel from "../../organisms/carousel/carousel"
-import Footer from "../../organisms/footer/footer"
-import Header from "../../organisms/header/header"
+const EXCHANGE_RATES = gql`
+{
+  countries {
+    name
+    code
+  }
+}
+`;
 
-const Homepage = ({ banner, carousel, footer, header }) => (
-  <>
-    <Header {...header} />
-    <Banner {...banner} />
-    <Carousel {...carousel} />
-    <Footer {...footer} />
-  </>
-)
-export default Homepage
+const Homepage = ({ banner, carousel, footer, header }) => {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <>
+      <Header {...header} />
+      {data.countries.map(country => (
+        <option key={country.code} value={country.code}>
+          {country.name}
+        </option>
+      ))}
+      <Banner {...banner} />
+      <Carousel {...carousel} />
+      <Footer {...footer} />
+    </>
+  );
+};
+export default Homepage;
